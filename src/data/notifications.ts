907f7564805ce38374f2dc6b62e94e5953dda227
Laetitia Uwumukiza <uwumukizalaetitia@gmail.com>
2024-11-13 13:58:13 +0200

    feat: add navbar component

---
+++ src/data/notifications.ts
@@ -0,0 +1,27 @@
+const notifications = [
+  {
+    title: 'Edit your information in a swipe',
+    description:
+      'Sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.',
+    date: '12 May, 2025',
+  },
+  {
+    title: 'It is a long established fact',
+    description: 'That a reader will be distracted by the readable.',
+    date: '24 Feb, 2025',
+  },
+  {
+    title: 'There are many variations',
+    description:
+      'Of passages of Lorem Ipsum available, but the majority have suffered.',
+    date: '04 Jan, 2025',
+  },
+  {
+    title: 'There are many variations',
+    description:
+      'Of passages of Lorem Ipsum available, but the majority have suffered.',
+    date: '01 Dec, 2024',
+  },
+];
+
+export default notifications;
