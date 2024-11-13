6f9fe3193e08c299cd2c29f00e3e6279e6b3b7d3
Laetitia Uwumukiza <uwumukizalaetitia@gmail.com>
2024-11-13 11:47:36 +0200

    feat: add sidebar component

---
+++ src/data/menu.ts
@@ -0,0 +1,84 @@
+import {
+  AuthenticationIcon,
+  CalendarIcon,
+  ChartIcon,
+  DashboardIcon,
+  FormsIcon,
+  ProfileIcon,
+  SettingsIcon,
+  TablesIcon,
+  UiElementIcon,
+} from '../images/sidebar';
+
+const sidebarItems = [
+  {
+    title: 'MENU',
+    links: [
+      {
+        label: 'Dashboard',
+        to: '/',
+        icon: DashboardIcon,
+        subLinks: [{ label: 'eCommerce', to: '/' }],
+      },
+      {
+        label: 'Calendar',
+        to: '/calendar',
+        icon: CalendarIcon,
+      },
+      {
+        label: 'Profile',
+        to: '/profile',
+        icon: ProfileIcon,
+      },
+      {
+        label: 'Forms',
+        to: '#',
+        icon: FormsIcon,
+        subLinks: [
+          { label: 'Form Elements', to: '/forms/form-elements' },
+          { label: 'Form Layout', to: '/forms/form-layout' },
+        ],
+      },
+      {
+        label: 'Tables',
+        to: '/tables',
+        icon: TablesIcon,
+      },
+      {
+        label: 'Settings',
+        to: '/settings',
+        icon: SettingsIcon,
+      },
+    ],
+  },
+  {
+    title: 'OTHERS',
+    links: [
+      {
+        label: 'Chart',
+        to: '/chart',
+        icon: ChartIcon,
+      },
+      {
+        label: 'UI Elements',
+        to: '#',
+        icon: UiElementIcon,
+        subLinks: [
+          { label: 'Alerts', to: '/ui/alerts' },
+          { label: 'Buttons', to: '/ui/buttons' },
+        ],
+      },
+      {
+        label: 'Authentication',
+        to: '#',
+        icon: AuthenticationIcon,
+        subLinks: [
+          { label: 'Sign In', to: '/auth/signin' },
+          { label: 'Sign Up', to: '/auth/signup' },
+        ],
+      },
+    ],
+  },
+];
+
+export default sidebarItems;
